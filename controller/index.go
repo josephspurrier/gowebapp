51604c8677efd3e9ee4818d40c156ba4ceeb8496
Joseph Spurrier <github@josephspurrier.com>
2015-08-28 21:06:17 -0400

    Added 500 handler

--- controller/index.go
+++ controller/index.go
@@ -35,6 +35,12 @@ func Error404(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Not Found 404")
 }
 
+// Error500 handles 500 - Internal Server Error
+func Error500(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprint(w, "Internal Server Error 500")
+}
+
 // InvalidToken handles CSRF attacks
 func InvalidToken(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
