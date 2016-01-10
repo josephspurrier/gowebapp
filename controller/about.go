51e8ff635c7df8cea698b192d2bd96ad4411ae3e
Joseph Spurrier <github@josephspurrier.com>
2016-01-10 13:26:04 -0500

    Update about.go

--- controller/about.go
+++ controller/about.go
@@ -6,7 +6,7 @@ import (
 	"github.com/josephspurrier/gowebapp/shared/view"
 )
 
-// Displays the default home page
+// Displays the About page
 func AboutGET(w http.ResponseWriter, r *http.Request) {
 	// Display the view
 	v := view.New(r)
